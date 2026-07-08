7f7b708cbbc343b3ea72dc27d7c00a15583a1653
Akky Parmar <akky.shory@gmail.com>
2026-07-08 22:14:54 +0530

    Add admin and executive app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 import ExecutiveApp from "./components/ExecutiveApp";
 
 function App() {
-  return <ExecutiveApp />;
+  const params = new URLSearchParams(window.location.search);
+  const view = params.get("view");
+
+  if (view === "executive") {
+    return <ExecutiveApp />;
+  }
+
+  return <Dashboard />;
 }
 
 export default App;
